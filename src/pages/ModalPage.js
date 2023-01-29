b2e3b2cb25d29611c5b898bb776f9dc072f3511d
petp91 <115895086+petp91@users.noreply.github.com>
2023-01-29 13:23:41 +0100

    vytvořena složka: components, pages, context a hooks

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -1,6 +1,6 @@
 import Modal from "../components/Modal";
 import Button from "../components/Button";
-import {useState} from "react";
+import { useState } from "react";
 const ModalPage = () => {
     const [showModal, setShowModal] = useState(false)
 
@@ -8,10 +8,48 @@ const ModalPage = () => {
         setShowModal(true)
     }
 
+    const handleClose = () => {
+        setShowModal(false)
+    }
+
+    const actionBar = <div>
+        <Button onClick={handleClose}  primary>I Accept</Button>
+    </div>
+
+    const modal = <Modal onClose={handleClose} actionBar={actionBar} >
+        <p>
+            Here is ........
+        </p>
+    </Modal>
+
     return (
-        <div>
+        <div className="relative">
             <Button onClick={handleClick} primary>Open Modal</Button>
-            {showModal && <Modal/>}
+            {showModal && modal}
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce posuere tellus sed quam imperdiet, posuere sollicitudin lectus tempor. Nunc sollicitudin fermentum cursus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin sit amet arcu hendrerit, interdum sapien porttitor, tempus lectus. Nullam imperdiet vulputate elementum. Nulla diam dui, placerat et elit sit amet, iaculis tristique dolor. Aenean porttitor purus felis, nec accumsan magna pellentesque a.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce posuere tellus sed quam imperdiet, posuere sollicitudin lectus tempor. Nunc sollicitudin fermentum cursus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin sit amet arcu hendrerit, interdum sapien porttitor, tempus lectus. Nullam imperdiet vulputate elementum. Nulla diam dui, placerat et elit sit amet, iaculis tristique dolor. Aenean porttitor purus felis, nec accumsan magna pellentesque a.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce posuere tellus sed quam imperdiet, posuere sollicitudin lectus tempor. Nunc sollicitudin fermentum cursus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin sit amet arcu hendrerit, interdum sapien porttitor, tempus lectus. Nullam imperdiet vulputate elementum. Nulla diam dui, placerat et elit sit amet, iaculis tristique dolor. Aenean porttitor purus felis, nec accumsan magna pellentesque a.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce posuere tellus sed quam imperdiet, posuere sollicitudin lectus tempor. Nunc sollicitudin fermentum cursus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin sit amet arcu hendrerit, interdum sapien porttitor, tempus lectus. Nullam imperdiet vulputate elementum. Nulla diam dui, placerat et elit sit amet, iaculis tristique dolor. Aenean porttitor purus felis, nec accumsan magna pellentesque a.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce posuere tellus sed quam imperdiet, posuere sollicitudin lectus tempor. Nunc sollicitudin fermentum cursus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin sit amet arcu hendrerit, interdum sapien porttitor, tempus lectus. Nullam imperdiet vulputate elementum. Nulla diam dui, placerat et elit sit amet, iaculis tristique dolor. Aenean porttitor purus felis, nec accumsan magna pellentesque a.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce posuere tellus sed quam imperdiet, posuere sollicitudin lectus tempor. Nunc sollicitudin fermentum cursus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin sit amet arcu hendrerit, interdum sapien porttitor, tempus lectus. Nullam imperdiet vulputate elementum. Nulla diam dui, placerat et elit sit amet, iaculis tristique dolor. Aenean porttitor purus felis, nec accumsan magna pellentesque a.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce posuere tellus sed quam imperdiet, posuere sollicitudin lectus tempor. Nunc sollicitudin fermentum cursus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin sit amet arcu hendrerit, interdum sapien porttitor, tempus lectus. Nullam imperdiet vulputate elementum. Nulla diam dui, placerat et elit sit amet, iaculis tristique dolor. Aenean porttitor purus felis, nec accumsan magna pellentesque a.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce posuere tellus sed quam imperdiet, posuere sollicitudin lectus tempor. Nunc sollicitudin fermentum cursus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin sit amet arcu hendrerit, interdum sapien porttitor, tempus lectus. Nullam imperdiet vulputate elementum. Nulla diam dui, placerat et elit sit amet, iaculis tristique dolor. Aenean porttitor purus felis, nec accumsan magna pellentesque a.
+            </p>
         </div>
     )
 }
